ae005774062fd15bd2ece78e4b1fe9bebfb2f463
matichj1179 <101526906+matichj1179@users.noreply.github.com>
2022-03-16 13:09:51 +1300

    Add files via upload

--- 02_variables.py
+++ 02_variables.py
@@ -5,7 +5,10 @@ greeting = "hello world"
 
 #they can also be numbers
 fav_num = 7
-print(fav_num *2)
 
-num_string= '5'
-print(num_string * 5)
+# Below is a constant ( the name is in uppercase)
+PI = 3.14
+
+num_2 = 2
+
+
