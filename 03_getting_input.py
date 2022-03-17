f483c2c9e16aa619eeae1b2b3cb17467baf35c10
matichj1179 <101526906+matichj1179@users.noreply.github.com>
2022-03-18 10:41:36 +1300

    Add files via upload

---
+++ 03_getting_input.py
@@ -0,0 +1,14 @@
+# Ask the user for their name
+username = input("Whats is your name?")
+
+# Ask the user for their favorite integer
+fav_num = int(input("Favorite Number?"))
+
+# Double, halve and sqaure the number
+double = fav_num * 2
+half = fav_num / 2
+sqaured = fav_num * fav_num
+
+print()
+# Greet the user
+print("Hi {}, your favorite number is {}".format9(username, fav_num))
