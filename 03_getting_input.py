a7611b3a03d0fe77e3488bf3de006b3e20af7f48
matichj1179 <101526906+matichj1179@users.noreply.github.com>
2022-03-18 10:53:34 +1300

    Add files via upload

--- 03_getting_input.py
+++ 03_getting_input.py
@@ -11,4 +11,11 @@ sqaured = fav_num * fav_num
 
 print()
 # Greet the user
-print("Hi {}, your favorite number is {}".format9(username, fav_num))
+print("Hi {}, your favorite number is {}".format(username, fav_num))
+print()
+# Output the results of doudling,halving
+#and sqauring their favorite number
+print("double {} is {}".format(fav_num, double))
+print("half {} is {}".format(fav_num, half))
+print("{} sqaured is {}".format(fav_num, sqaured))
+print()
